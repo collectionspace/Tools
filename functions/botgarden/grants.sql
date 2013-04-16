55b63edeb2e82948b6ce296b2c544edd035e782f
Chris Hoffman <chris.hoffman@berkeley.edu>
2013-04-16 13:11:21 -0700

    UCBG grants to functions updated

--- functions/botgarden/grants.sql
+++ functions/botgarden/grants.sql
@@ -1,7 +1,6 @@
 grant execute on function findcommonname(character varying) to public;
-grant execute on function findhybridnamehtml(character varying) to public;
-grant execute on function findhybridname(character varying) to public;
 grant execute on function findhybridaffinname(character varying) to public;
+grant execute on function findhybridaffinhtml(character varying) to public;
 grant execute on function findsectionparent(character varying) to public;
 grant execute on function findsubsectionparent(character varying) to public;
 grant execute on function findvoucherlabels() to public;
