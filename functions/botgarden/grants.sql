2a1fdb8b0fdfcda5c9e78484604f1bcf55f82db8
Chris Hoffman <chris.hoffman@berkeley.edu>
2013-04-16 10:57:03 -0700

    Bot Garden functions and reports
    incorporating functions that get affinity and hybrid names (plain and
    html-formatted)
    deleting old functions
    updating Taxon Count report

--- functions/botgarden/grants.sql
+++ functions/botgarden/grants.sql
@@ -1,7 +1,7 @@
 grant execute on function findcommonname(character varying) to public;
 grant execute on function findhybridnamehtml(character varying) to public;
 grant execute on function findhybridname(character varying) to public;
+grant execute on function findhybridaffinname(character varying) to public;
 grant execute on function findsectionparent(character varying) to public;
 grant execute on function findsubsectionparent(character varying) to public;
-– voucherlabeltype.sql: types don't need grants
 grant execute on function findvoucherlabels() to public;
