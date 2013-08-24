1738ae53b30e66a597ae2b0826ad433a5e06ea80
jblowe <jblowe@berkeley.edu>
2013-08-23 20:41:10 -0700

    PAHMA-930, PAHMA-926, PAHMA-811: fieldsets in KIR/OIR, handler update, object count now updates.

--- webapps/py/cswaMain.py
+++ webapps/py/cswaMain.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import cgi
+import traceback
 import cgitb; cgitb.enable()  # for troubleshooting
 from cswaUtils import *
 from cswaObjDetails import *
@@ -33,7 +34,11 @@ if not action:
 # if location2 was not specified, default it to location1
 if str(form.get('lo.location2')) == '':
     form['lo.location2'] = form.get('lo.location1')
-    
+
+# same for objects
+if str(form.get('ob.objno2')) == '':
+    form['ob.objno2'] = form.get('ob.objno1')
+
 if updateType == 'packinglist' and action == 'Download as CSV':  
     downloadCsv(form,config)
     sys.exit(0)
@@ -43,65 +48,73 @@ else:
     print starthtml(form,config)
 
 elapsedtime = time.time()
-sys.stdout.flush()
+try:
+    sys.stdout.flush()
 
-if checkServer == 'check server':
-    print serverCheck(form,config)
-else:
-    if action == "Enumerate Objects":
-        doEnumerateObjects(form,config)
-    elif action == "Create Labels for Locations Only":
-        doBarCodes(form,config)
-    elif action == config.get('info','updateactionlabel'):
-        if   updateType == 'packinglist':  doPackingList(form,config)
-        elif updateType == 'movecrate':    doUpdateLocations(form,config)
-        elif updateType == 'barcodeprint': doBarCodes(form,config)
-        elif updateType == 'inventory':    doUpdateLocations(form,config)
-        elif updateType == 'moveobject':   doUpdateLocations(form,config)
-        elif updateType == 'objinfo':      doUpdateKeyinfo(form,config)
-        elif updateType == 'keyinfo':      doUpdateKeyinfo(form,config)
-        elif updateType == 'bedlist':      doBedList(form,config)
-        # elif updateType == 'holdings':     doBedList(form,config)
-        # elif updateType == 'locreport':    doBedList(form,config)
-        elif updateType == 'advsearch':    doAdvancedSearch(form,config)
-        elif updateType == 'upload':       uploadFile(actualform,config)
-        elif action == "Recent Activity":
-            viewLog(form,config)
-##    # special case: if only one location in range, jump to enumerate
-##    elif form.getvalue("lo.location1") != '' and str(form.getvalue("lo.location1")) == str(form.getvalue("lo.location2")) :
-##        if updateType in ['keyinfo', 'inventory']: 
-##            doEnumerateObjects(form,config)
-##        elif updateType == 'movecrate':
-##            doCheckMove(form,config)
-##        else:
-##            doLocationSearch(form,config,'nolist')
-    elif action == "Search":
-        if   updateType == 'packinglist':  doLocationSearch(form,config,'nolist')
-        elif updateType == 'movecrate':    doCheckMove(form,config)
-        elif updateType == 'barcodeprint':
-            if form.get('ob.objectnumber'):
-                #form['action'] = 'Create Label for this Object'
-                doSingleObjectSearch(form, config)
-            else:
-                doLocationSearch(form, config, 'nolist')
-        elif updateType == 'bedlist':      doComplexSearch(form,config,'select')
-        elif updateType == 'holdings':     doAuthorityScan(form,config)
-        elif updateType == 'locreport':    doAuthorityScan(form,config)
-        elif updateType == 'advsearch':    doComplexSearch(form,config,'select')
-        elif updateType == 'inventory':    doLocationSearch(form,config,'list')
-        elif updateType == 'keyinfo':      doLocationSearch(form,config,'list')
-        elif updateType == 'objinfo':      doObjectSearch(form,config,'list')
-        elif updateType == 'moveobject':   doObjectSearch(form,config,'list')
-        elif updateType == 'objdetails':   doObjectDetails(form,config)
-
-    elif action == "View Hierarchy":
-        doHierarchyView(form,config)
-    
-    elif action in ['<<','>>']:
-        print "<h3>Sorry not implemented yet! Please try again tomorrow!</h3>"
+    if checkServer == 'check server':
+        print serverCheck(form,config)
     else:
-        pass
-        #print "<h3>Unimplemented action %s!</h3>" % str(action)
+        if action == "Enumerate Objects":
+            doEnumerateObjects(form,config)
+        elif action == "Create Labels for Locations Only":
+            doBarCodes(form,config)
+        elif action == config.get('info','updateactionlabel'):
+            if   updateType == 'packinglist':  doPackingList(form,config)
+            elif updateType == 'movecrate':    doUpdateLocations(form,config)
+            elif updateType == 'barcodeprint': doBarCodes(form,config)
+            elif updateType == 'inventory':    doUpdateLocations(form,config)
+            elif updateType == 'moveobject':   doUpdateLocations(form,config)
+            elif updateType == 'objinfo':      doUpdateKeyinfo(form,config)
+            elif updateType == 'keyinfo':      doUpdateKeyinfo(form,config)
+            elif updateType == 'bedlist':      doBedList(form,config)
+            # elif updateType == 'holdings':     doBedList(form,config)
+            # elif updateType == 'locreport':    doBedList(form,config)
+            elif updateType == 'advsearch':    doAdvancedSearch(form,config)
+            elif updateType == 'upload':       uploadFile(actualform,config)
+            elif action == "Recent Activity":
+                viewLog(form,config)
+    ##    # special case: if only one location in range, jump to enumerate
+    ##    elif form.getvalue("lo.location1") != '' and str(form.getvalue("lo.location1")) == str(form.getvalue("lo.location2")) :
+    ##        if updateType in ['keyinfo', 'inventory']:
+    ##            doEnumerateObjects(form,config)
+    ##        elif updateType == 'movecrate':
+    ##            doCheckMove(form,config)
+    ##        else:
+    ##            doLocationSearch(form,config,'nolist')
+        elif action == "Search":
+            if   updateType == 'packinglist':  doLocationSearch(form,config,'nolist')
+            elif updateType == 'movecrate':    doCheckMove(form,config)
+            elif updateType == 'barcodeprint':
+                if form.get('ob.objectnumber'):
+                    #form['action'] = 'Create Label for this Object'
+                    doSingleObjectSearch(form, config)
+                else:
+                    doLocationSearch(form, config, 'nolist')
+            elif updateType == 'bedlist':      doComplexSearch(form,config,'select')
+            elif updateType == 'holdings':     doAuthorityScan(form,config)
+            elif updateType == 'locreport':    doAuthorityScan(form,config)
+            elif updateType == 'advsearch':    doComplexSearch(form,config,'select')
+            elif updateType == 'inventory':    doLocationSearch(form,config,'list')
+            elif updateType == 'keyinfo':      doLocationSearch(form,config,'list')
+            elif updateType == 'objinfo':      doObjectSearch(form,config,'list')
+            elif updateType == 'moveobject':   doObjectSearch(form,config,'list')
+            elif updateType == 'objdetails':   doObjectDetails(form,config)
+
+        elif action == "View Hierarchy":
+            doHierarchyView(form,config)
+
+        elif action in ['<<','>>']:
+            print "<h3>Sorry not implemented yet! Please try again tomorrow!</h3>"
+        else:
+            pass
+            #print "<h3>Unimplemented action %s!</h3>" % str(action)
+
+except:
+    sys.stderr.write("error! %s" % traceback.format_exc())
+    print '''<h3><span style="color:red;">Sorry! An error occurred; it has been logged and will be investigated.<br/>
+        However, to expedite matters, please contact John Lowe jblowe@berkeley.edu or Michael Black mtblack@berkeley.edu.
+        Please record the time and what you were doing when this unfortunate event happened.
+        '</span></h3>'''
 
 elapsedtime = time.time() - elapsedtime
 
