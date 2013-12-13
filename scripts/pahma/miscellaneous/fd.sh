748ac014fd411fea11cbd1c72a669fbd7e7709d7
jblowe <jblowe@berkeley.edu>
2013-12-13 09:33:35 -0800

    PAHMA-916: a small kit to track currentlocation issues

--- scripts/pahma/miscellaneous/fd.sh
+++ scripts/pahma/miscellaneous/fd.sh
@@ -1,1 +1,1 @@
-time psql -t -U reporter -d "host=pahma.cspace.berkeley.edu dbname=nuxeo password=csR2p4rt2r" -f finddiffs.sql | mail -s "computed vs actual locations" mtblack@berkeley.edu
+time psql -t -U reporter -d "host=pahma.cspace.berkeley.edu dbname=nuxeo password=xxxxxxxx" -f finddiffs.sql | mail -s "computed vs actual locations" mtblack@berkeley.edu
