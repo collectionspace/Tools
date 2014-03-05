8a603bfb5b72bf542ba57ea440ace456139aa0d0
jblowe <jblowe@berkeley.edu>
2014-03-04 16:38:25 -0800

    NOJIRA: sample deploy script for CGI webapps.

---
+++ webapps/py/deployCGIscripts.sh
@@ -0,0 +1,16 @@
+#
+# sample script to deploy webapps from this GitHub directory on a local Linux VM
+#
+# NB: UNTESTED! SAMPLE ONLY! PROBABLY DOES NOT WORK AS IS!
+#
+# copy files to approrpriate directories, keeping dir structure intact
+#
+cp cswa*.py           /var/www/cgi-bin/
+cp autosuggest.py     /var/www/cgi-bin/
+cp -r ../extras/*.js  /var/www/js/
+cp -r ../extras/css/*.css /var/www/css/
+cp -r ../extras/*.png /var/www/images/
+cp -r ../extras/*.jpg /var/www/images/
+cp -r ../extras/*.gif /var/www/icons/
+#
+# what about .cfg files...?
