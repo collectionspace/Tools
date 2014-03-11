33ecca955d868c6c99afc371e7e966a2472d9259
JulianJaffe <julianjaffe@berkeley.edu>
2014-03-11 10:59:50 -0700

    NOJIRA: fixes to deployCGIscript.py
    Added checks to ensure that target directories exist (could be replaced
    by changing cp to rsync?). Script now sets some permissions (not sure if
    more necessary)

--- webapps/py/deployCGIscripts.sh
+++ webapps/py/deployCGIscripts.sh
@@ -3,9 +3,24 @@
 #
 # NB: UNTESTED! SAMPLE ONLY! PROBABLY DOES NOT WORK AS IS!
 #
-# copy files to approrpriate directories, keeping dir structure intact
+# copy files to appropriate directories, keeping dir structure intact
 #
 git pull origin $1 -v
+if [ ! -d /var/www/cgi-bin/ ]; then
+    mkdir -p /var/www/cgi-bin/
+fi
+if [ ! -d /var/www/js/ ]; then
+    mkdir -p /var/www/js/
+fi
+if [ ! -d /var/www/css/ ]; then
+    mkdir -p /var/www/css/
+fi
+if [ ! -d /var/www/images/ ]; then
+    mkdir -p /var/www/images/
+fi
+if [ ! -d /var/www/icons/ ]; then
+    mkdir -p /var/www/icons/
+fi
 cp cswa*.py           /var/www/cgi-bin/
 cp autosuggest.py     /var/www/cgi-bin/
 cp -r ../extras/*.js  /var/www/js/
@@ -14,6 +29,10 @@ cp -r ../extras/*.png /var/www/images/
 cp -r ../extras/*.jpg /var/www/images/
 cp -r ../extras/*.svg /var/www/images/
 cp -r ../extras/*.gif /var/www/icons/
+
+cd /var/www/cgi-bin/
+chmod a+x cswaMain.py
+cd .. && chgrp -R developers cswa*.py
 
 #Don't know about the other files, i.e.
 #cp checkPlace.py      /var/www/cgi-bin/
