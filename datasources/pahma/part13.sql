b20cf3e0010c77f1c3ae3416de770cfb6d1a825a
Michael Black <mtblack@berkeley.edu>
2014-06-19 16:39:40 -0700

    PAHMA-1085: added begin and end dates and eras for production date

--- datasources/pahma/part13.sql
+++ datasources/pahma/part13.sql
@@ -1,4 +1,6 @@
-SELECT DISTINCT cc.id, spd.datedisplaydate AS "objproddate_txt"
+SELECT DISTINCT cc.id, spd.datedisplaydate AS "objproddate_txt",
+DATE(spd.dateearliestscalarvalue) AS "objproddate_begin_dt", spd.dateearliestsingleera AS "objproddate_beginera_txt",
+DATE(spd.datelatestscalarvalue) AS "objproddate_end_dt", spd.datelatestera AS "objproddate_endera_txt"
 FROM collectionobjects_common cc
 JOIN hierarchy hpd ON (hpd.parentid=cc.id AND hpd.primarytype='structuredDateGroup' AND hpd.name='collectionobjects_common:objectProductionDateGroupList' AND (hpd.pos=0 or hpd.pos IS NULL))
 JOIN structureddategroup spd ON (spd.id=hpd.id)
