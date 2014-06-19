c7a4191168cca5113630ae956fbbae3051566283
Michael Black <mtblack@berkeley.edu>
2014-06-19 16:31:15 -0700

    PAHMA-1085: added begin and end dates and eras for collection date

--- datasources/pahma/part12.sql
+++ datasources/pahma/part12.sql
@@ -1,4 +1,6 @@
-SELECT cc.id, scd.datedisplaydate AS "objcolldate_txt"
+SELECT cc.id, scd.datedisplaydate AS "objcolldate_txt",
+DATE(scd.dateearliestscalarvalue) AS "objcolldate_begin_dt", scd.dateearliestsingleera AS "objcolldate_beginera_txt",
+DATE(scd.datelatestscalarvalue) AS "objcolldate_end_dt", scd.datelatestera AS "objcolldate_endera_txt"
 FROM collectionobjects_common cc
 JOIN hierarchy hcd ON (hcd.parentid=cc.id AND hcd.primarytype='structuredDateGroup' AND hcd.name='collectionobjects_pahma:pahmaFieldCollectionDateGroupList' AND (hcd.pos=0 or hcd.pos IS NULL))
 JOIN structureddategroup scd ON (scd.id=hcd.id)
