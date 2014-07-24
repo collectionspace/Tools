468d46e51f4eda6f135c4239d10742d7046a33b5
jblowe <jblowe@berkeley.edu>
2014-07-24 09:29:13 -0700

    UCBG-380: correcting sql changes

--- datasources/botgarden/botgardenMetadataV1alive.sql
+++ datasources/botgarden/botgardenMetadataV1alive.sql
@@ -67,8 +67,7 @@ end as locality_s,
 h1.name as csid_s,
 con.rare as rare_s,
 cob.deadflag as deadflag_s,
-cob.flowerColor as flowerColor_s,
-co.fieldCollectionNumber as fieldCollectionNumber_s,
+cob.flowercolor as flowercolor_s,
 regexp_replace(tig2.taxon, '^.*\)''(.*)''$', '\1') as determinationNoAuth_s,
 mc.reasonformove as reasonformove_s
 
