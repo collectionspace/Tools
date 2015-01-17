46200bc3712ab8e3eb0a5550953de69850879847
John Lowe <jblowe@berkeley.edu>
2015-01-17 13:22:35 -0800

    NOJIRA: refined version of deployment script.

--- webapps/py/deployCGIscripts.sh
+++ webapps/py/deployCGIscripts.sh
@@ -1,26 +1,30 @@
 #
 # sample script to deploy webapps from this GitHub directory on a local Linux VM
 #
-# NB: UNTESTED! SAMPLE ONLY! PROBABLY DOES NOT WORK AS IS!
+# copy files to appropriate directories
 #
-# copy files to appropriate directories, keeping dir structure intact
+if [ -z "$1" ]
+  then
+    echo "Please specify GitHub branch as an argument"
+    exit 1
+fi
+
+error=0
+for f in cgi-bin js css images icons
+do
+    if [ ! -d /var/www/$f/ ]; then
+        echo "/var/www/$f does not exist, please create it"
+        error=1
+    fi
+done
+
+if [ $error == 1 ]
+  then
+    echo "Please correct the directory issues above and try again."
+    exit
+fi
 #
 git pull origin $1 -v
-if [ ! -d /var/www/cgi-bin/ ]; then
-    mkdir -p /var/www/cgi-bin/
-fi
-if [ ! -d /var/www/js/ ]; then
-    mkdir -p /var/www/js/
-fi
-if [ ! -d /var/www/css/ ]; then
-    mkdir -p /var/www/css/
-fi
-if [ ! -d /var/www/images/ ]; then
-    mkdir -p /var/www/images/
-fi
-if [ ! -d /var/www/icons/ ]; then
-    mkdir -p /var/www/icons/
-fi
 cp cswa*.py           /var/www/cgi-bin/
 cp autosuggest.py     /var/www/cgi-bin/
 cp -r ../extras/*.js  /var/www/js/
