6203cadaab09a82977f8765ba6e2da170e2fc059
John Lowe <jblowe@berkeley.edu>
2015-02-05 20:34:44 -0800

    NOJIRA: getting rid of superfluous file.

--- reports/helpers/fr.sh
+++
@@ -1,1 +0,0 @@
-curl -u "admin@botgarden.cspace.berkeley.edu:bgNPtochange" -X POST "https://botgarden.cspace.berkeley.edu/cspace-services/reports/$1" -H "Content-Type:application/xml" -T payload.xml > $2
