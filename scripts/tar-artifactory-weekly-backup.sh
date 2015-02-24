a78db484c04835472907bb504c87b02005848388
Aron Roberts <aron@socrates.berkeley.edu>
2015-02-24 13:40:35 -0800

    CSPACE-6443: Regex fine-tuning. Avoid proceeding if any temporary backup directory is found, to avoid potentially deleting the last known good backup. Add status messages throughout.

--- scripts/tar-artifactory-weekly-backup.sh
+++ scripts/tar-artifactory-weekly-backup.sh
@@ -34,16 +34,40 @@ ARTIFACTORY_WEEKLY_BACKUP_DIR=/var/lib/artifactory/backup/backup-weekly
 # Get a list of all the weekly backup directories.
 #
 COUNT=0
+TMP_DIR_REGEX='.*/[0-9]{8}\.[0-9]{6}\.tmp$'
+BACKUP_DIR_REGEX='.*/[0-9]{8}\.[0-9]{6}'
+
 for file in $ARTIFACTORY_WEEKLY_BACKUP_DIR/*
 do
     if [[ -d $file ]]; then
-        # For each directory encountered, check that it 1) has a name
-        # matching Artifactory's weekly backup pattern (e.g. "20150218.030000"),
-        # and 2) is non-empty.
-        if [[ "$file" =~ .*?/[0-9]{8}\.[0-9]{6}$ ]] && [ "$(ls -A $file)" ]; then
+        #
+        # If any temporary directory whose name matches Artifactory's
+        # weekly backup temporary directory name pattern
+        # (e.g. "20150218.030000.tmp") is found, then quit.
+        #
+        # This likely indicates that some backup was incomplete,
+        # and it isn't safe to proceed further without human review.
+        #
+        if [[ "$file" =~ $TMP_DIR_REGEX ]]; then
+            echo "Temporary directory (from a possibly incomplete Artifactory backup)"
+            echo "was found."
+            echo "Not sure how to proceed: no action was taken."
+            exit 1
+        fi
+        #
+        # For every other directory encountered, check that it:
+        # 1) has a name matching Artifactory's weekly backup name pattern
+        # (e.g. "20150218.030000"); and
+        # 2) is non-empty.
+        #
+        # If it does, add it to the list of backup directories found.
+        #
+        if [[ "$file" =~ $BACKUP_DIR_REGEX ]] && [ "$(ls -A $file)" ]; then
             let COUNT=COUNT+1
+            #
             # If a directory fulfills both of those criteria, add it to an array
             # of directory paths
+            #
             dirlist[$COUNT]=$file
         fi
     fi
@@ -53,10 +77,15 @@ done
 # Get the count of Artifactory weekly backup directories found.
 #
 let DIRCOUNT=${#dirlist[@]}
-echo "$DIRCOUNT backup directories were found ..."
+#
+# Uncomment for debugging:
+#
+# echo "$DIRCOUNT backup directories were found ..."
 
+#
 # If there are no weekly backup directories found, then quit.
-if [ "$DIRCOUNT" -le 0 ]; then
+#
+if [ "$DIRCOUNT" -eq 0 ]; then
   echo "No backup directories were found ..."
   exit 0
 fi
@@ -82,20 +111,29 @@ fi
 #
 # Otherwise ...
 #
+# (Note that the relevant entry in the dirlist array will be
+# at index position 1, not 0, per the 'do' loop above.)
+#
+CURRENT_WEEKLY_BACKUP_DIR=${dirlist[1]}
+echo "Current weekly backup directory found at $CURRENT_WEEKLY_BACKUP_DIR ..."
+
+#
 # Delete any existing tarred and/or tarred and gzipped backups
 # in this directory.
 #
 unamestr=`uname`
 if [[ "$unamestr" == "Linux" ]]; then
-    find $ARTIFACTORY_WEEKLY_BACKUP_DIR -regextype posix-egrep -regex ".*/[0-9]{8}\.[0-9]{6}\.tar" -delete
-    find $ARTIFACTORY_WEEKLY_BACKUP_DIR -regextype posix-egrep -regex ".*/[0-9]{8}\.[0-9]{6}\.tar\.gz" -delete
+    echo "Looking for older tarred backups to delete, to free up disk space ..."
+    find $ARTIFACTORY_WEEKLY_BACKUP_DIR -regextype posix-egrep -regex ".*/[0-9]{8}\.[0-9]{6}\.tar" -delete -print
+    find $ARTIFACTORY_WEEKLY_BACKUP_DIR -regextype posix-egrep -regex ".*/[0-9]{8}\.[0-9]{6}\.tar\.gz" -delete -print
 elif [[ "$unamestr" == "Darwin" ]]; then
-    find -E $ARTIFACTORY_WEEKLY_BACKUP_DIR -regex ".*/[0-9]{8}\.[0-9]{6}\.tar" -delete
-    find -E $ARTIFACTORY_WEEKLY_BACKUP_DIR -regex ".*/[0-9]{8}\.[0-9]{6}\.tar\.gz" -delete
+    echo "Looking for older tarred backups to delete, to free up disk space ..."
+    find -E $ARTIFACTORY_WEEKLY_BACKUP_DIR -regex ".*/[0-9]{8}\.[0-9]{6}\.tar" -delete -print
+    find -E $ARTIFACTORY_WEEKLY_BACKUP_DIR -regex ".*/[0-9]{8}\.[0-9]{6}\.tar\.gz" -delete -print
 fi
 
 #
-# Tar the most recent week's backup directory
+# Tar up the current week's backup directory
 #
-CURRENT_WEEKLY_BACKUP_DIR=${dirlist[1]}
+echo "Tarring up the current week's backup directory ..."
 tar -cf $CURRENT_WEEKLY_BACKUP_DIR.tar $CURRENT_WEEKLY_BACKUP_DIR
