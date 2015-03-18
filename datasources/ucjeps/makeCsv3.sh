419e275a0a0d2a44b7d8fd665e1cd9a3bece2556
John Lowe <jblowe@berkeley.edu>
2015-03-18 09:05:08 -0700

    UCJEPS-564: revisions to Solr ETL to support new fields

--- datasources/ucjeps/makeCsv3.sh
+++ datasources/ucjeps/makeCsv3.sh
@@ -9,28 +9,62 @@ USERNAME="reporter_ucjeps"
 DATABASE="ucjeps_domain_ucjeps"
 CONNECTSTRING="host=$HOSTNAME dbname=$DATABASE password=$PASSWORD"
 ##############################################################################
-# extract metadata (dead and alive) info from CSpace
+# extract and massage the metadata from CSpace
 ##############################################################################
 time psql -F $'\t' -R"@@" -A -U $USERNAME -d "$CONNECTSTRING" -f ucjepsMetadataV2.sql -o d1.csv
-# some fix up required, alas: data from cspace is dirty: contain csv delimiters, newlines, etc. that's why we used @@ as temporary record separator
 time perl -pe 's/[\r\n]/ /g;s/\@\@/\n/g' d1.csv > d3.csv 
-time perl -ne '$x = $_ ;s/[^\t]//g; if (length eq 41) { print $x;} '     d3.csv > d4.csv
-time perl -ne '$x = $_ ;s/[^\t]//g; unless (length eq 41) { print $x;} ' d3.csv > errors.csv &
-mv d4.csv metadata.csv
-time psql -F $'\t' -R"@@" -A -U $USERNAME -d "$CONNECTSTRING" -f ucjepsMediaV1.sql -o m1.csv
-time perl -pe 's/[\r\n]/ /g;s/\@\@/\n/g' m1.csv > media.csv
-rm m1.csv d1.csv d3.csv
-# add the blobcsids to the rest of the data
+time perl -ne '$x = $_ ;s/[^\t]//g; if (length eq 49) { print $x;} '     d3.csv > metadata.csv
+time perl -ne '$x = $_ ;s/[^\t]//g; unless (length eq 49) { print $x;} ' d3.csv > errors_in_field_counts.csv &
+##############################################################################
+# make a unique sequence number for id
+##############################################################################
+perl -i -pe '$i++;print $i . "\t"' metadata.csv
+##############################################################################
+# add the blobcsids to mix (expects to read media.csv and metadata.csv)
+##############################################################################
 time perl mergeObjectsAndMedia.pl > d6.csv
+##############################################################################
 # we want to use our "special" solr-friendly header.
+##############################################################################
 tail -n +2 d6.csv | perl fixdate.pl > d7.csv
-cat metadataHeaderV3.csv d7.csv > 4solr.$TENANT.metadata.csv
-rm d6.csv d7.csv m1.csv d1.csv d3.csv
+##############################################################################
+# check latlongs
+##############################################################################
+perl -ne '@x=split /\t/;print if abs($x[22])<90 && abs($x[23])<180;' d7.csv > d8.csv
+perl -ne '@x=split /\t/;print if !(abs($x[22])<90 && abs($x[23])<180);' d7.csv > errors_in_latlong.csv
+##############################################################################
+# recover & use our "special" solr-friendly header, which got buried
+##############################################################################
+head -1 metadata.csv > header4Solr.csv
+##############################################################################
+# add the blob field name to the header, name the first column 'id'
+##############################################################################
+perl -i -pe 's/$/\tblob_ss/;s/^1\t/id\t/' header4Solr.csv
+grep -v csid_s d8.csv > d9.csv
+cat header4Solr.csv d9.csv | perl -pe 's/␥/|/g' > 4solr.$TENANT.metadata.csv
+##############################################################################
+# here are the schema changes needed: copy all the _s and _ss to _txt, and vv.
+##############################################################################
+perl -pe 's/\t/\n/g' header4Solr.csv| perl -ne 'chomp; next unless /_txt/; s/_txt$//; print "    <copyField source=\"" .$_."_txt\" dest=\"".$_."_s\"/>\n"' > schemaFragment.xml
+perl -pe 's/\t/\n/g' header4Solr.csv| perl -ne 'chomp; next unless /_s$/; s/_s$//; print "    <copyField source=\"" .$_."_s\" dest=\"".$_."_txt\"/>\n"' >> schemaFragment.xml
+perl -pe 's/\t/\n/g' header4Solr.csv| perl -ne 'chomp; next unless /_ss$/; s/_ss$//; print "    <copyField source=\"" .$_."_ss\" dest=\"".$_."_txt\"/>\n"' >> schemaFragment.xml
+##############################################################################
+# here are the solr csv update parameters needed for multivalued fields
+##############################################################################
+perl -pe 's/\t/\n/g' header4Solr.csv| perl -ne 'chomp; next unless /_ss/; next if /blob/; print "f.$_.split=true&f.$_.separator=%7C&"' > uploadparms.txt
+##############################################################################
+#rm d?.csv m?.csv
+##############################################################################
 wc -l *.csv
+##############################################################################
 # clear out the existing data
+##############################################################################
 curl "http://localhost:8983/solr/${TENANT}-metadata/update" --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+##############################################################################
+# load the csv file into Solr using the csv DIH
+##############################################################################
 curl "http://localhost:8983/solr/${TENANT}-metadata/update" --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
-time curl "http://localhost:8983/solr/${TENANT}-metadata/update/csv?commit=true&header=true&trim=true&separator=%09&f.previousdeterminations_ss.split=true&f.previousdeterminations_ss.separator=;&f.associatedtaxa_ss.split=true&f.associatedtaxa_ss.separator=;&f.typeassertions_ss.split=true&f.typeassertions_ss.separator=;&f.othernumbers_ss.split=true&f.othernumbers_ss.separator=;&f.blobs_ss.split=true&f.blobs_ss.separator=,&encapsulator=\\" --data-binary @4solr.$TENANT.metadata.csv -H 'Content-type:text/plain; charset=utf-8'
+time curl "http://localhost:8983/solr/${TENANT}-metadata/update/csv?commit=true&header=true&trim=true&separator=%09&f.previousdeterminations_ss.split=true&f.previousdeterminations_ss.separator=%7C&f.associatedtaxa_ss.split=true&f.associatedtaxa_ss.separator=%7C&f.typeassertions_ss.split=true&f.typeassertions_ss.separator=%7C&f.othernumber_ss.split=true&f.othernumber_ss.separator=%7C&f.blobs_ss.split=true&f.blobs_ss.separator=,&encapsulator=\\" --data-binary @4solr.$TENANT.metadata.csv -H 'Content-type:text/plain; charset=utf-8'
 #
 rm 4solr*.csv.gz
 gzip 4solr.*.csv
--- datasources/ucjeps/ucjepsMetadataV2.sql
+++ datasources/ucjeps/ucjepsMetadataV2.sql
@@ -1,15 +1,18 @@
 select
-    h1.name as CSID,
-    co.objectnumber as AccessionNumber,
+    h1.name as csid_s,
+    co.objectnumber as accessionnumber_s,
     case when (tig.taxon is not null and tig.taxon <> '')
                 then regexp_replace(regexp_replace(tig.taxon, '^.*\)''(.*)''$', '\1'),E'[\\t\\n\\r]+', ' ', 'g')
-    end as Determination,
-    tu.taxonmajorgroup as MajorGroup,
+    end as determination_s,
+    ttg.termformatteddisplayname as termformatteddisplayname_s,
+    regexp_replace(regexp_replace(tnh.family, '^.*\)''(.*)''$', '\1'),E'[\\t\\n\\r]+', ' ', 'g') as family_s,
+    tnh.taxonbasionym as taxonbasionym_s,
+    tu.taxonmajorgroup as majorgroup_s,
     case when (fc.item is not null and fc.item <> '')
                 then regexp_replace(regexp_replace(fc.item, '^.*\)''(.*)''$', '\1'),E'[\\t\\n\\r]+', ' ', 'g')
-    end as Collector,
-    co.fieldcollectionnumber as CollectorNumber,
-    sdg.datedisplaydate as CollectionDate,
+    end as collector_s,
+    co.fieldcollectionnumber as collectornumber_s,
+    sdg.datedisplaydate as collectiondate_s,
     case
         when
             sdg.dateearliestsingleyear != 0
@@ -22,7 +25,7 @@ select
             sdg.dateearliestsingleday::varchar(2),
             'yyyy-mm-dd')
         else null
-    end as EarlyCollectionDate,
+    end as earlycollectiondate_dt,
     case
         when
             sdg.datelatestyear != 0
@@ -35,32 +38,36 @@ select
             sdg.datelatestday::varchar(2),
             'yyyy-mm-dd')
         else null
-    end as LateCollectionDate,
-    regexp_replace(lg.fieldlocverbatim,E'[\\t\\n\\r]+', ' ', 'g') as Locality,
-    lg.fieldloccounty as CollCounty,
-    lg.fieldlocstate as CollState,
-    lg.fieldloccountry as CollCountry,
-    lg.velevation as Elevation,
-    lg.minelevation as MinElevation,
-    lg.maxelevation as MaxElevation,
-    lg.elevationunit as ElevationUnit,
-    regexp_replace(co.fieldcollectionnote,E'[\\t\\n\\r]+', ' ', 'g') as Habitat,
-    lg.decimallatitude as DecLatitude,
-    lg.decimallongitude as DecLongitude,
+    end as latecollectiondate_dt,
+    regexp_replace(lg.fieldlocverbatim,E'[\\t\\n\\r]+', ' ', 'g') as locality_s,
+    lg.fieldloccounty as collcounty_s,
+    lg.fieldlocstate as collstate_s,
+    lg.fieldloccountry as collcountry_s,
+    lg.velevation as elevation_s,
+    lg.minelevation as minelevation_s,
+    lg.maxelevation as maxelevation_s,
+    lg.elevationunit as elevationunit_s,
+    regexp_replace(co.fieldcollectionnote,E'[\\t\\n\\r]+', ' ', 'g') as habitat_s,
+    lg.decimallatitude as location_0_coordinate,
+    lg.decimallongitude as location_1_coordinate,
+    lg.decimallatitude || ', ' || lg.decimallongitude as latlong_p,
     case when lg.vcoordsys like 'Township%'
                 then lg.vcoordinates
-    end as TRSCoordinates,
-    lg.geodeticdatum as Datum,
-    lg.localitysource as CoordinateSource,
-    lg.coorduncertainty as CoordinateUncertainty,
-    lg.coorduncertaintyunit as CoordinateUncertaintyUnit,
-    cc.updatedat as UpdatedAt,
+    end as trscoordinates_s,
+    lg.geodeticdatum as datum_s,
+    lg.localitysource as coordinatesource_s,
+    lg.coorduncertainty as coordinateuncertainty_f,
+    lg.coorduncertaintyunit as coordinateuncertaintyunit_s,
+    lg.localitynote as localitynote_s,
+    lg.localitysource as localitysource_s,
+    lg.localitysourcedetail as localitysourcedetail_s,
+    cc.updatedat as updatedat_dt,
     case when conh.labelheader like 'urn:%' then getdispl(conh.labelheader)
         else conh.labelheader
-    end as Label_Header,
+    end as labelheader_s,
     case when conh.labelfooter like 'urn:%' then getdispl(conh.labelfooter)
         else conh.labelfooter
-    end as Label_Footer,
+    end as labelfooter_s,
     array_to_string(array
       (SELECT CASE WHEN (tig2.taxon IS NOT NULL AND tig2.taxon <>'' and tig2.taxon not like '%no name%') THEN (getdispl(tig2.taxon) 
 	||CASE WHEN (tig2.identby IS NOT NULL AND tig2.identby <>'' and tig2.identby not like '%unknown%') THEN ', by ' || getdispl(tig2.identby) ELSE '' END
@@ -74,13 +81,13 @@ select
         left outer join taxonomicIdentGroup tig2 on (tig2.id = htig2.id)
         left outer join hierarchy hprevdet on (tig2.id = hprevdet.parentid and hprevdet.name = 'identDateGroup')
         left outer join structureddategroup prevdetsdg on (prevdetsdg.id = hprevdet.id)
-       where h1int.name=h1.name order by htig2.pos), '␥', '') Previous_Determinations,
-    lng.localname as Local_Name,
-    case when cocbd.item is null or cocbd.item = '' then null else cocbd.item end as Brief_Description,
-    lg.vdepth as Depth,
-    lg.mindepth as MinDepth,
-    lg.maxdepth as MaxDepth,
-    lg.depthunit as DepthUnit,
+       where h1int.name=h1.name order by htig2.pos), '␥', '') previousdeterminations_ss,
+    lng.localname as localname_s,
+    case when cocbd.item is null or cocbd.item = '' then null else cocbd.item end as briefdescription_txt,
+    lg.vdepth as depth_s,
+    lg.mindepth as mindepth_s,
+    lg.maxdepth as maxdepth_s,
+    lg.depthunit as depthUnit_s,
     array_to_string(array
       (SELECT CASE WHEN (atg.associatedtaxon IS NOT NULL AND atg.associatedtaxon<>'') THEN (getdispl(atg.associatedtaxon) 
 	||CASE WHEN (atg.interaction IS NOT NULL AND atg.interaction<>'') THEN ' (' || atg.interaction||')' ELSE '' END) ELSE '' END
@@ -90,7 +97,7 @@ select
         and hatg.name = 'collectionobjects_naturalhistory:associatedTaxaGroupList')
       left outer join associatedtaxagroup atg on (hatg.id = atg.id)
       where h4int.name = h1.name
-      order by hatg.pos), '␥', '') Associated_Taxa,    
+      order by hatg.pos), '␥', '') as associatedtaxa_ss,
     array_to_string(array
       (SELECT CASE WHEN (tsg.typespecimenkind IS NOT NULL AND tsg.typespecimenkind <>'') THEN (tsg.typespecimenkind 
 	||CASE WHEN (tsg.typespecimenbasionym IS NOT NULL AND tsg.typespecimenbasionym <>'') THEN ' (' || getdispl(tsg.typespecimenbasionym)||')' ELSE '' END) ELSE '' END
@@ -100,10 +107,10 @@ select
         and htsg.name = 'collectionobjects_naturalhistory:typeSpecimenGroupList')
        left outer join typespecimengroup tsg on (tsg.id = htsg.id)
        where h2int.name = h1.name
-       order by htsg.pos), '␥', '') Type_Assertions,
-    case when conh.cultivated is null or conh.cultivated = '' then null else conh.cultivated end as Cultivated,
-    case when co.sex is null or co.sex = '' then null else co.sex end as Sex,
-    co.phase as Phase,
+       order by htsg.pos), '␥', '') as typeassertions_ss,
+    case when conh.cultivated is null or conh.cultivated = '' then null else conh.cultivated end as Cultivated_s,
+    case when co.sex is null or co.sex = '' then null else co.sex end as sex_s,
+    co.phase as phase_s,
     array_to_string(array
       (SELECT CASE WHEN (ong.numbervalue IS NOT NULL AND ong.numbervalue<>'') THEN (ong.numbervalue 
 	||CASE WHEN (ong.numbertype IS NOT NULL AND ong.numbertype <>'') THEN ' (' || ong.numbertype||')' ELSE '' END) ELSE '' END
@@ -113,11 +120,12 @@ select
          and hong.name = 'collectionobjects_common:otherNumberList')
        left outer join othernumber ong on (ong.id = hong.id)
        where h3int.name = h1.name
-       order by hong.pos), '␥', '') Other_Numbers,
+       order by hong.pos), '␥', '') as othernumber_ss,
+    'ucbgacccession' as ucbgaccessionnumber_s,
     CASE WHEN (tig.identby IS NOT NULL AND tig.identby <>'' and tig.identby not like '%unknown%') THEN (getdispl(tig.identby) 
 	||CASE WHEN (tig.institution IS NOT NULL AND tig.institution <>'') THEN ', ' || getdispl(tig.institution) ELSE '' END
 	||CASE WHEN (detdetailssdg.datedisplaydate IS NOT NULL AND detdetailssdg.datedisplaydate <>'' and detdetailssdg.datedisplaydate <>' ') THEN ', ' || detdetailssdg.datedisplaydate ELSE '' END
-	||CASE WHEN (tig.identkind IS NOT NULL AND tig.identkind <>'') THEN ' (' || tig.identkind || ')' ELSE '' END) ELSE '' END AS Determination_Details
+	||CASE WHEN (tig.identkind IS NOT NULL AND tig.identkind <>'') THEN ' (' || tig.identkind || ')' ELSE '' END) ELSE '' END AS determinationdetails_s
 from collectionobjects_common co
 inner join misc on co.id = misc.id
 inner join hierarchy h1 on co.id = h1.id
@@ -137,7 +145,13 @@ left outer join hierarchy hlg
         on (co.id = hlg.parentid and hlg.pos = 0
         and hlg.name = 'collectionobjects_naturalhistory:localityGroupList')
 left outer join taxon_common tc on (tig.taxon = tc.refname)
+left outer join hierarchy httg on (
+    tc.id = httg.parentid
+    and httg.name = 'taxon_common:taxonTermGroupList'
+    and httg.pos = 0)
+left outer join taxontermgroup ttg on (ttg.id = httg.id)
 left outer join taxon_ucjeps tu on (tu.id = tc.id)
+left outer join taxon_naturalhistory tnh on (tnh.id = tc.id)
 left outer join localitygroup lg on (lg.id = hlg.id)
 left outer join collectionobjects_naturalhistory conh on (co.id = conh.id)
 left outer join hierarchy hlng on (co.id = hlng.parentid and hlng.primarytype = 'localNameGroup' and hlng.pos = 0)
@@ -145,7 +159,7 @@ left outer join localNameGroup lng on (hlng.id = lng.id)
 left outer join collectionobjects_common_briefdescriptions cocbd on (co.id = cocbd.id and cocbd.pos = 0)
 where misc.lifecyclestate <> 'deleted'
 -- and h1.name = '3380bad9-5bea-4eed-860e' -- UCcrhtest on ucjeps-dev
--- and h1.name = '338075de-821c-49b3-8f34-969cc666a61e' -- JEPS46872 
+-- and h1.name = '338075de-821c-49b3-8f34-969cc666a61e' -- JEPS46872
 -- and h1.name = '33803cfe-e6a8-4025-bf53-a3814cf4da82'	-- JEPS105623
 -- and h1.name like '3380%'
 and substring(co.objectnumber from '^[A-Z]*') in ('UC', 'UCLA', 'JEPS', 'GOD')
