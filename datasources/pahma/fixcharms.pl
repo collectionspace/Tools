8fb508e2fbcbb26a3e6146e1219f82b13bf116e7
John Lowe <jblowe@berkeley.edu>
2015-05-02 10:00:25 -0700

    PAHMA-1241: make images for funerary objects restricted.

--- datasources/pahma/fixcharms.pl
+++ datasources/pahma/fixcharms.pl
@@ -1,11 +1,12 @@
 use strict;
 
-# replace the blob csid with the csid of the 'restricted image' if the letters c-h-a-r-m appear in the objectname
+# restrict images of charmstones and funerary objects
+# replace the blob csid with the csid of the 'restricted image' if the regex appears anywheren in the row
 while (<>) {
   chomp;
   my (@columns) = split /\t/,$_,-1;
   #print "bef:\t" . $columns[10] . "\t" . $columns[37] . "\n";
-  @columns[37] = '59a733dd-d641-4e1a-8552' if ((@columns[10] =~ /charm/i) && @columns[37]);
+  @columns[37] = '59a733dd-d641-4e1a-8552' if ($_ =~ /(charm.*stone|funerary)/i && @columns[37]);
   #print "aft:\t" . $columns[10] . "\t" . $columns[37] . "\n";
   #print scalar @columns, "\n";
   print join("\t",@columns). "\n";
