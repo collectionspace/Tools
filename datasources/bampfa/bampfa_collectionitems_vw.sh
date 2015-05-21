faa28dda681d315a6c32aece723d98156e6c2b66
John Lowe <jblowe@berkeley.edu>
2015-05-21 09:31:39 -0700

    BAMPFA-433: make the file .tab instead of .csv

--- datasources/bampfa/bampfa_collectionitems_vw.sh
+++ datasources/bampfa/bampfa_collectionitems_vw.sh
@@ -12,11 +12,11 @@ CONNECTSTRING="host=$SERVER dbname=$DATABASE"
 ##############################################################################
 # 
 ##############################################################################
-time psql -R"@@" -A -U $USERNAME -d "$CONNECTSTRING"  -c "select * from utils.${TENANT}_collectionitems_vw" -o ${TENANT}_collectionitems_vw.csv
+time psql -R"@@" -A -U $USERNAME -d "$CONNECTSTRING"  -c "select * from utils.${TENANT}_collectionitems_vw" -o ${TENANT}_collectionitems_vw.tab
 # some fix up required, alas: data from cspace is dirty: contain csv delimiters, newlines, etc. that's why we used @@ as temporary record separator
-time perl -i -pe 's/[\r\n]/ /g;s/\@\@/\n/g;s/\|/\t/g;' ${TENANT}_collectionitems_vw.csv
-rm ${TENANT}_collectionitems_vw.csv.gz
-gzip ${TENANT}_collectionitems_vw.csv
-mail -a ${TENANT}_collectionitems_vw.csv.gz -s "${TENANT}_collectionitems_vw.csv.gz" -- osanchez@berkeley.edu < /dev/null
+time perl -i -pe 's/[\r\n]/ /g;s/\@\@/\n/g;s/\|/\t/g;' ${TENANT}_collectionitems_vw.tab
+rm ${TENANT}_collectionitems_vw.tab.gz
+gzip ${TENANT}_collectionitems_vw.tab
+mail -a ${TENANT}_collectionitems_vw.tab.gz -s "${TENANT}_collectionitems_vw.csv.gz" -- osanchez@berkeley.edu < /dev/null
 #
 date
