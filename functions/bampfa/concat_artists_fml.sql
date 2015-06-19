73c6830c3da3d83c30dfa145d647df436553d250
Lam Voong <lkv@berkeley.edu>
2015-06-19 11:23:29 -0700

    Update concat_artists_fml.sql to include grant statement and test query.

--- functions/bampfa/concat_artists_fml.sql
+++ functions/bampfa/concat_artists_fml.sql
@@ -1,3 +1,6 @@
+-- DROP FUNCTION utils.concat_artists_fml (csid VARCHAR);
+
+-- Concatenates names of artists associated with a collection object in 'first middle lastname' format.
  CREATE OR REPLACE FUNCTION utils.concat_artists_fml(csid character varying)
   RETURNS character varying
   LANGUAGE plpgsql
@@ -41,3 +44,28 @@
  END;
  
  $function$
+
+GRANT EXECUTE ON FUNCTION utils.concat_artists_fml (csid VARCHAR) TO PUBLIC;
+
+/*
+select coc.objectnumber, utils.concat_artists_fml(hcoc.name)
+from collectionobjects_common coc
+inner join hierarchy hcoc on (coc.id = hcoc.id)
+where coc.objectnumber in (
+    'EL.2.00.3',
+    '1995.46.432.6',
+    '2005.14.81',
+    '1994.13.3',
+    '1995.46.184');
+    
+ 2005.14.81    | T.R. Uthco; Doug Hall; Diane Andrews Hall; Jody Procter; Ant Farm; Chip Lord; Doug 
+Michels; Curtis Schreier
+ 1995.46.432.6 | 
+ 1995.46.184   | Nova Scotia College of Art and Design; Iain Baxter; Eleanor Beveridge; Gerald Fergu
+son; Hisako Hamada; George Kokis; Alyce Orehover; Paul Hrusovsky; Robert DeGaetano; Joseph Kosuth; J
+im Leedy; Jim Melchert; P. Rada; Peter Voulkos; Mary B. Yates
+ 1994.13.3     | D-L Alvarez; Joan Jett Black; Nayland Blake; Erin Courtney; Mary Ewert; Vincent Fec
+teau; Mark Gonzales; Donna Han; Cliff Hengst; Philip Horvitz; David E. Johnson; Kevin Killian; John 
+Lindell; Connell Ray Little; Keith Mayerson; Michelle Rollman; Wayne Smith; Jim Winters
+ EL.2.00.3     | Philip Galle; Pieter Bruegel I
+*/
