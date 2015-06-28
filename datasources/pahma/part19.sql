c82f8d8936d0f7fa67ce9a1b4672926bddd6d50e
John Lowe <jblowe@berkeley.edu>
2015-06-28 10:26:55 -0700

    PAHMA-1315: fix bug in location+crate+donor+condition query

--- datasources/pahma/part19.sql
+++ datasources/pahma/part19.sql
@@ -7,7 +7,7 @@ SELECT
 
 FROM collectionobjects_common cc
   JOIN hierarchy h1 ON (h1.id = cc.id)
-  JOIN collectionobjects_pahma cp ON (cp.id = cc.id)
-  JOIN collectionobjects_naturalhistory conh ON (cc.id = conh.id)
+  JOIN collectionobjects_pahma cp ON (cc.id = cp.id)
   JOIN collectionobjects_anthropology ca ON (cc.id = ca.id)
+  FULL OUTER JOIN collectionobjects_naturalhistory conh ON (cc.id = conh.id)
   JOIN misc ON (cc.id = misc.id AND misc.lifecyclestate <> 'deleted')
