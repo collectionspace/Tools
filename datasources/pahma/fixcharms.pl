1681ff668282e1c5f5689bbb3bd4e3bfc61b6432
John Lowe <jblowe@berkeley.edu>
2015-08-14 22:49:57 -0700

    PAHMA-1361: add 'North America' to conditions

--- datasources/pahma/fixcharms.pl
+++ datasources/pahma/fixcharms.pl
@@ -9,7 +9,7 @@ while (<>) {
   # use placeholder image if certain text appears anwhere in the record
   @columns[37] = '59a733dd-d641-4e1a-8552' if ($_ =~ /(charm.*stone|NAGPRA-associated Funerary Objects)/i && @columns[37]);
   # use placeholder image if context of use field contains the word burial
-  @columns[37] = '59a733dd-d641-4e1a-8552' if (@columns[15] =~ /burial/i && @columns[37]);
+  @columns[37] = '59a733dd-d641-4e1a-8552' if (@columns[15] =~ /burial/i && @columns[36] =~ /North America/i && @columns[37]);
   #print "aft:\t" . $columns[10] . "\t" . $columns[37] . "\n";
   #print scalar @columns, "\n";
   print join("\t",@columns). "\n";
