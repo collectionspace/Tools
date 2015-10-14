e0cdcc3bf4188111aff2cb551883a9887b62e175
John Lowe <jblowe@berkeley.edu>
2015-10-13 18:22:34 -0700

    PAHMA-1408: revised latlong offsets

--- datasources/pahma/obfuscateUSArchaeologySites.py
+++ datasources/pahma/obfuscateUSArchaeologySites.py
@@ -2,10 +2,25 @@ import csv
 import hashlib
 import math
 import sys
+import numpy as np
 
+hashkey_column = 10
 fieldCollectionTree_column = 36
 objecttype_column = 5
 latlong_column = 34
+
+
+def cart2pol(x, y):
+    rho = np.sqrt(x ** 2 + y ** 2)
+    phi = np.arctan2(y, x)
+    return (rho, phi)
+
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return (x, y)
+
 
 with open(sys.argv[2], "wb") as out:
     writer = csv.writer(out, delimiter="\t")
@@ -17,23 +32,31 @@ with open(sys.argv[2], "wb") as out:
                     # obfuscate lat-long
 
                     # first, get the actual values...
-                    latitude = row[latlong_column].split(",")[0]
+                    latitude = row[latlong_column].split(",")[0].strip()
                     longitude = row[latlong_column].split(",")[1].strip()
-                    # 'fieldCollectionTree_column' is the constant we will be hashing with
+
+                    # read PAHMA-1408 for details of why this is the way it is
+                    # 'hashkey_column' is the constant we will be hashing with
                     # (we want to hash to the same value each run, lest someone try to 'zero in' using multiple
                     # observations of the portal data...)
-                    location = row[fieldCollectionTree_column]
+                    location = row[hashkey_column]
+                    modulus = 0.2
 
                     # get md5 hash of secret value, convert to int, normalize this to range of -.05 to .05 degrees
                     lat_offset = int(hashlib.md5(location).hexdigest(), 16)
                     lat_offset = (lat_offset + 0.0) / int("9" * len(str(lat_offset)))  # Clamp value to 0 to 1
-                    latitude = float(latitude) + (lat_offset - 0.5) / 10
+                    lat_offset = (lat_offset % modulus) / modulus
 
-                    # reverse the location for hash of longitude
                     long_offset = int(hashlib.md5(location[::-1]).hexdigest(), 16)
                     long_offset = (long_offset + 0.0) / int("9" * len(str(long_offset)))  # Clamp value to 0 to 1
-                    longitude = float(longitude) + (long_offset - 0.5) / 10
+                    long_offset = (long_offset % modulus) / modulus
 
+                    # pretend these a polar coordinates and convert them to cartesian coordinates
+                    latlongoffset = pol2cart(np.sqrt(lat_offset), long_offset * 2 * math.pi)
+                    latlongoffset = [r * 0.05 for r in latlongoffset]
+
+                    latitude = float(latitude) + latlongoffset[0]
+                    longitude = float(longitude) + latlongoffset[1]
                     row[latlong_column] = "%s,%s" % (latitude, longitude)
             except:
                 print 'problem!!!'
