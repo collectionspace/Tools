75991df0f46cb3bbee650d9d174aca4e5a896ffc
John Lowe <jblowe@berkeley.edu>
2016-02-03 16:04:02 -0800

    UCJEPS-614: revisions to query to get ALL media.

--- datasources/ucjeps/ucjepsNewMedia.sql
+++ datasources/ucjeps/ucjepsNewMedia.sql
@@ -20,7 +20,7 @@ FROM media_common mc
 JOIN media_ucjeps mu on (mc.id=mu.id)
 JOIN misc ON (mc.id = misc.id AND misc.lifecyclestate <> 'deleted')
 LEFT OUTER JOIN hierarchy h1 ON (h1.id = mc.id)
-JOIN relations_common r on (h1.name = r.objectcsid)
+LEFT OUTER JOIN relations_common r on (h1.name = r.objectcsid)
 LEFT OUTER JOIN hierarchy h2 on (r.subjectcsid = h2.name)
 LEFT OUTER JOIN collectionobjects_common cc on (h2.id = cc.id)
 LEFT OUTER JOIN collectionobjects_ucjeps cop on (h2.id = cop.id)
