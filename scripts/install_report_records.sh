d9fd6bc8bc26ed6cb4e8e6d222ecca7e54d5d5cd
remillet <remillet@yahoo.com>
2016-02-09 16:19:32 -0800

    CSPACE-6848: Updated scripts to deal with multiple canned reports.

---
+++ scripts/install_report_records.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# This script requires 8 arguments to build a proper POST request to the
+# CollectionSpace RESTFul API.
+# 
+# The 'filename' argument (the 8th and last argument), needs to be the name of the
+# JasperReports (http://community.jaspersoft.com) report file that is installed in
+# the 'cspace/reports' directory of CollectionSpace.  For more information about CollectionSpace
+# reports, visit this wiki page: https://wiki.collectionspace.org/display/DOC/How+to+add+and+run+reports
+#
+#<document name="reports">
+#	<ns2:reports_common xmlns:ns2="http://collectionspace.org/services/report">
+#		<name>The name of the report shown in the UI</name>		# 1st argument
+#		<notes>Just a few fields about the report</notes>		# 2nd argument
+#		<forDocTypes>
+#			<forDocType>Acquisition</forDocType>				# 3rd argument
+#		</forDocTypes>
+#		<supportsSingleDoc>true</supportsSingleDoc>				# 4th argument
+#		<supportsDocList>false</supportsDocList>				# 5th argument
+#		<supportsGroup>false</supportsGroup>					# 6th argument
+#		<supportsNoContext>true</supportsNoContext>				# 7th argument
+#		<filename>acq_basic.jrxml</filename>					# 8th argument
+#		<outputMIME>application/pdf</outputMIME>				# Gets filled out by 'create_report_records' invokation
+#	</ns2:reports_common>
+#</document>
+#
+
+NEWLINE=$'\n'
+echo "$NEWLINE"
+
+declare -i ERRORS_COUNTER=0
+declare -a ERRORS_MSGS
+
+function test {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+		msg=$"Error executing $@."
+		ERRORS_MSGS[ERRORS_COUNTER]="$msg"
+		ERRORS_COUNTER+=1		
+    fi
+    return $status
+}
+
+test ./create-report-records.sh "Acquisition Summary" "An Acquisition Summary report" Acquisition true false false true acq_basic.jrxml
+test ./create-report-records.sh "Acquisition Basic List" "Catalog info for objects related to an acquisition record" Acquisition true false false true Acq_List_Basic.jrxml
+test ./create_report_records.sh "Condition Check Basic List" "Catalog info for objects related to a condition check record" Conditioncheck true false false true CC_List_Basic.jrxml
+test ./create_report_records.sh "Exhibition Basic List" "Catalog info for objects related to a exhibition record" Exhibition true false false true Exhibition_List_Basic.jrxml
+test ./create_report_records.sh "Group Basic List" "Catalog info for objects related to a group record" Group true false false true Group_List_Basic.jrxml
+test ./create_report_records.sh "Loan-in Basic List" "Catalog info for objects related to a Loan-in record" Loanin true false false true LoansIn_List_Basic.jrxml
+test ./create_report_records.sh "Loan-out Basic List" "Catalog info for objects related to a Loan-out record" Loanout true false false true LoansOut_List_Basic.jrxml
+
+
+#curl -G -v http://localhost:8180/cspace-services/reports --data-urlencode "as=reports_common:name ILIKE 'Acquisition Summary%'" -u admin@core.collectionspace.org:Administrator
+
+if [ $ERRORS_COUNTER -gt 0 ]; then
+	echo
+	echo "### Errors Summary: $ERRORS_COUNTER error(s)."
+	declare -i count=0
+	while [ $count -lt $ERRORS_COUNTER ]
+	do
+		echo ${ERRORS_MSGS[count]} >&2
+		count+=1
+	done
+fi
