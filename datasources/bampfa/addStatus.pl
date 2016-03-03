561ed7324715beba65f3819031dce83e4c3a1d9d
John Lowe <jblowe@berkeley.edu>
2016-03-02 17:46:05 -0800

    BAMPFA-412: implement decisions on public locations

--- datasources/bampfa/addStatus.pl
+++ datasources/bampfa/addStatus.pl
@@ -1,10 +1,17 @@
 @cell = split /\|/;
 $status = $cell[37];
-$status = "Study Center" if $status =~ /Study Center/i;
-$status = "Gallery"      if $status =~ /Gallery/i;
-$status = "Not on view"  unless $status =~ /(Study Center|Gallery)/;
-# for now, until locations are set, everything is 'not on view'
+# BAMPFA-412
+# "Asian Study"* => "located in Asian Study Center"
+# "Study Center*" => "located in Art Study Centers"
+# Ditto for "Gallery*", "Reading Room*", "Community Gallery*" = "On view"
+# Everything else is "Not on view"
+#
+# ok, ok, yes there is an implied if-then-else hidden here...I think it's fine, at least for now.
 $status = "Not on view";
+$status = "located in Art Study Center" if $status =~ /Study Center/i;
+$status = "located in Asian Study Center" if $status =~ /Asian Study/i;
+$status = "On View" if $status =~ /(Gallery|Reading Room|Community Gallery)*/i;
 $i++;
 $status = "status" if $i == 1;
+# add to tail end of record, vertical bar is delimiter here.
 s/$/\|$status/;
