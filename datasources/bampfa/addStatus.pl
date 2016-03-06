a77ec8c58b4dbeef84ae602b714034b1ea6c8fc0
John Lowe <jblowe@berkeley.edu>
2016-03-05 21:16:43 -0800

    BAMPFA-412: compute 'status' field, based on regexes over locations

--- datasources/bampfa/addStatus.pl
+++ datasources/bampfa/addStatus.pl
@@ -1,5 +1,7 @@
+#
+# this needs to be run with the -p option, e.g. perl -p addStatus.pl d4.csv > metadata.csv
 @cell = split /\|/;
-$status = $cell[37];
+$location = $cell[37];
 # BAMPFA-412
 # "Asian Study"* => "located in Asian Study Center"
 # "Study Center*" => "located in Art Study Centers"
@@ -8,9 +10,11 @@ $status = $cell[37];
 #
 # ok, ok, yes there is an implied if-then-else hidden here...I think it's fine, at least for now.
 $status = "Not on view";
-$status = "located in Art Study Center" if $status =~ /Study Center/i;
-$status = "located in Asian Study Center" if $status =~ /Asian Study/i;
-$status = "On View" if $status =~ /(Gallery|Reading Room|Community Gallery)*/i;
+$status = "located in Art Study Center" if $location =~ /.*Study Center.*/i;
+$status = "located in Asian Study Center" if $location =~ /.*Asian Study.*/i;
+$status = "On View" if $location =~ /.*(Gallery|Reading Room|Community Gallery).*/i;
+# everything is not on view" unless we sort this process out
+$status = "Not On View";
 $i++;
 $status = "status" if $i == 1;
 # add to tail end of record, vertical bar is delimiter here.
