b3159749acf55ddb71460b0c46fb31f5bf577cb3
John Lowe <jblowe@berkeley.edu>
2016-03-22 14:22:26 -0700

    PAHMA-1220: revise image restriction calculation

--- datasources/pahma/mergeObjectsAndMediaPAHMA.pl
+++ datasources/pahma/mergeObjectsAndMediaPAHMA.pl
@@ -67,13 +67,14 @@ while (<METADATA>) {
   $count{'metadata'}++;
   my $mediablobs;
   if ($blobs{$objectcsid}{'type'}) {
-    # if context of use field contains the word burial
-    $blobs{$objectcsid}{'images'} = $restricted if (@rest[12] =~ /burial/i && @rest[34] =~ /United States/i);
-    # if object name contains something like "charm stone"
-    $blobs{$objectcsid}{'images'} = $restricted if (@rest[8] =~ /charm.*stone/i && @rest[34] =~ /United States/i);
-    # belt-and-suspenders: restrict if charm stone or NAGPRA appear anywhere in USA records...
-    $blobs{$objectcsid}{'images'} = $restricted if ($_ =~ /(charm.*stone|NAGPRA-associated Funerary Objects)/i && @rest[34] =~ /United States/i);
-
+    if ($runtype eq 'public') {
+      # if context of use field contains the word burial
+      $blobs{$objectcsid}{'images'} = $restricted if (@rest[12] =~ /burial/i && @rest[34] =~ /United States/i);
+      # if object name contains something like "charm stone"
+      $blobs{$objectcsid}{'images'} = $restricted if (@rest[8] =~ /charm.*stone/i && @rest[34] =~ /United States/i);
+      # belt-and-suspenders: restrict if charm stone or NAGPRA appear anywhere in USA records...
+      $blobs{$objectcsid}{'images'} = $restricted if ($_ =~ /(charm.*stone|NAGPRA-associated Funerary Objects)/i && @rest[34] =~ /United States/i);
+    } 
     # insert list of blobs, etc. as final columns
     $blobs{$objectcsid}{'restrictions'} =~ s/,$//;
     $blobs{$objectcsid}{'type'} =~ s/,$//;
