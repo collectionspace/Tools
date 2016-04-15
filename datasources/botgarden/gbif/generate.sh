25d5fc258bd02b2b21fe6c7a307bf91022855fbd
John Lowe <jblowe@berkeley.edu>
2016-04-15 13:58:31 -0700

    NOJIRA: minor refactoring of UCBG GBIF parsing tools

--- datasources/botgarden/gbif/generate.sh
+++ datasources/botgarden/gbif/generate.sh
@@ -1,19 +1,22 @@
-cd /usr/local/share/django/botgarden_project/
-cd gbif
-cp /home/developers/botgarden/4solr.botgarden.metadata.csv.gz .
-cd /home/developers/botgarden
-gunzip 4solr.botgarden.metadata.csv.gz 
-mv 4solr.botgarden.metadata.csv botgarden.csv
-cut -f33 botgarden.csv > scinames.csv
-less scinames.csv 
-head -1 botgarden.csv 
-cut -f4 botgarden.csv > scinames.csv
-less scinames.csv 
-vi scinames.csv 
-python parseAndInsertGBIFparts.py 4solr.botgarden.metadata.csv output.csv names.pickle 3
-# python parseNamesGBIF4UCBG.py scinames.csv parsednames.csv names.pickle
+#
+# fragile script to regenerate the cache (pickle) of GBIF parsed name parts
+# 
+# invoke as: ./generate.sh
+#
+# if you want to regenerate the cache from scratch, erase names.pickle
+#
+cd ~/solrdatasources/botgarden/gbif
+cp ../4solr.botgarden.public.csv.gz .
+gunzip 4solr.botgarden.public.csv.gz 
+mv 4solr.botgarden.public.csv botgarden.csv
+# just to be clear: we are reparsing the determination field from the previous
+# night's extract
+cut -f17 botgarden.csv > scinames.csv
+# this version of the script is a bit rude: it hits GBIF sequentially, but without pauses
+python parseNamesGBIF4UCBG.py scinames.csv parsednames.csv names.pickle
 # nohup python /usr/local/share/django/botgarden_project/gbif/parseNamesGBIF4UCBG.py scinames.csv parsednames.csv names.pickle &
-#       python /usr/local/share/django/botgarden_project/gbif/parseNamesGBIF4UCBG.py scinames.csv parsednames.csv names.pickle
-cut -f15 parsednames.csv | less
-cut -f12 parsednames.csv | less
-cut -f12 parsednames.csv | sort | uniq -c
+# python /usr/local/share/django/botgarden_project/gbif/parseNamesGBIF4UCBG.py scinames.csv parsednames.csv names.pickle
+# cut -f15 parsednames.csv | less
+# cut -f12 parsednames.csv | less
+# cut -f12 parsednames.csv | sort | uniq -c
+rm botgarden.csv
