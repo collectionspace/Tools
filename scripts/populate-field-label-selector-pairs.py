962eae265d155ac2c2b4f83227b1e5ee3ce944e4
Aron Roberts <aron@socrates.berkeley.edu>
2016-05-16 20:37:17 -0700

    CQA-221: Refactored to start with list of available labels in the uispec. Hard-coded to acquistions; needs a minor fix for that. Also uncovered some possible irregularity in messages for at least acquisitions.

--- scripts/populate-field-label-selector-pairs.py
+++ scripts/populate-field-label-selector-pairs.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-# import ConfigParser
+from collections import OrderedDict
 import json
 import re
 import os
 import sys
 
+# From Jack Kelly
+# http://stackoverflow.com/a/3663505
+def rchop(thestring, ending):
+  if thestring.endswith(ending):
+    return thestring[:-len(ending)]
+  return thestring
+
+# From Bryukhanov Valentin
+# http://stackoverflow.com/a/12507546
+# (renamed and with minor changes to variable names)
+def list_generator_from_dict(current_dict, prefix=None):
+    prefix = prefix[:] if prefix else []
+    if isinstance(current_dict, dict):
+        for key, value in current_dict.items():
+            if isinstance(value, dict):
+                for d in list_generator_from_dict(value, [key] + prefix):
+                    yield d
+            elif isinstance(value, list) or isinstance(value, tuple):
+                for v in value:
+                    for d in list_generator_from_dict(v, [key] + prefix):
+                        yield d
+            else:
+                yield prefix + [key, value]
+    else:
+        yield current_dict
+        
+# http://stackoverflow.com/q/2436607
+def filterPick(list,filter):
+    return [ ( l, m.group(1) ) for l in list for m in (filter(l),) if m]
+        
 FIELD_PATTERN = re.compile("^\$\{fields\.(\w+)\}$", re.IGNORECASE)
 def get_field_name(value):
     # 'basestring' -> 'str' here in Python 3
@@ -25,6 +55,7 @@ def get_field_name(value):
             # print v
 
 MESSAGEKEY_KEY = 'messagekey'
+UNICODE_MESSAGEKEY_KEY = unicode(MESSAGEKEY_KEY)
 def get_messagekey_from_item(item):
     # 'basestring' -> 'str' here in Python 3
     if isinstance(item, dict):
@@ -133,37 +164,100 @@ if __name__ == '__main__':
     # ##################################################
     
     messagekeys = {}
-    for selector, value in sorted(uispec_items.iteritems()):
+    messagekeys_not_found_msgs = []
+    text_labels_not_found_msgs = []
+    for selector, value in uispec_items.iteritems():
         
-        # TODO:
-        # From a second look at the uispec file, there's likely
-        # merit in first looking for all labels, and then finding
-        # the fields that correspond to these labels (and getting
-        # the latters' selectors)
-        #
-        # That would entail a bit of refactoring here 
+        # For debugging
+        # print "%s %s\n" % (selector, value)
+        
         messagekey = get_messagekey_from_item(value)
         if messagekey is not None:
             try:
                 text_label = text_labels_lowercase[messagekey.lower()]
                 if not text_label.strip():
-                    print "// Could not find text label for message key '%s'" % messagekey
+                    text_labels_not_found_msgs.append("// Not found: text label for message key '%s'" % messagekey)
                 else:
                     # Strip leading '.' from selector
                     selector = selector.replace(selector[:1], '')
                     messagekeys[selector] = text_label
             except KeyError, e:
-                print "// Could not find message key '%s'" % messagekey
-        
-        for key, value in messagekeys.iteritems():
-            print 'fieldSelectorByLabel.put("%s", "%s");' % (value, key)
+                messagekeys_not_found_msgs.append("// Not found: message key '%s'" % messagekey)
+
+    # For debugging
+    # for key, value in messagekeys.iteritems():
+    #     print 'fieldSelectorByLabel.put("%s", "%s");' % (value, key)
             
-        # http://stackoverflow.com/a/12507546
-        # TODO:
-        # Walk the entire tree, get all field values, construct field selectors
-        # compare to the label selectors (sans '-label') and see where there
-        # are - and are not - matches. Output any non-matches, either of
-        # labels or fields, as comments.
+    generator = list_generator_from_dict(uispec[TOP_LEVEL_KEY])
+
+    selector_items = []
+    for uispec_item in generator:
+        # TODO: There may be more elegant and/or faster ways to do this
+        # with list comprehensions and/or fiters
+        if isinstance(uispec_item, list):
+            for u_item in uispec_item:
+                # TODO: Replace with regex to avoid unintentional matches
+                if str(u_item).startswith(".cs"):
+                    selector_items.append(uispec_item)
+    
+    # For debugging
+    # for s_item in selector_items:
+    #     print s_item
+    
+    field_items = []
+    field_regex = re.compile(".*fields\.", re.IGNORECASE)
+    for selector_item in selector_items:
+        for entry in selector_item:
+            if field_regex.match(entry):
+                field_items.append(selector_item)
+               
+    # For debugging
+    # for f_item in field_items:
+    #     print f_item
+                    
+    LABEL_SUFFIX = "-label"
+    #########
+    CSC_PREFIX = "csc-"
+    CSC_RECORD_TYPE_PREFIX = CSC_PREFIX + "acquisition-" # HARD_CODED!!! get_record_type_selector_prefix('any')
+    #########
+    CSC_RECORD_TYPE_PREFIX_LENGTH = len(CSC_RECORD_TYPE_PREFIX)
+    fields = {}
+    fields_not_found_msgs = []
+    for key, value in messagekeys.iteritems():
+        messagekey_fieldname = rchop(key, LABEL_SUFFIX)
+        if messagekey_fieldname.startswith(CSC_RECORD_TYPE_PREFIX):
+            messagekey_fieldname = messagekey_fieldname[CSC_RECORD_TYPE_PREFIX_LENGTH:]
+        found = False
+        print messagekey_fieldname
+        num_found = 0
+        for field_item in field_items:
+            for item in field_item:
+                messagekey_fieldname_regex = re.compile(".*fields\." + messagekey_fieldname, re.IGNORECASE)
+                if messagekey_fieldname_regex.match(str(item)):
+                    # The following needs work; the first item may not hold a selector
+                    # We need to find the item in a list that contains the 
+                    for possible_selector_item in field_item:
+                        if str(possible_selector_item).startswith("." + CSC_PREFIX):
+                            # For debugging
+                            # print "=> %s" % possible_selector_item
+                            fieldkey = possible_selector_item.replace(possible_selector_item[:1], '')
+                            fields[fieldkey] = value
+                            found = True
+        if not found:
+            fields_not_found_msgs.append("// Not found: field for label %s" % rchop(key, LABEL_SUFFIX))
+        
+    for key, value in sorted(fields.iteritems(), key=lambda (k,v): (v,k)):
+        print 'fieldSelectorByLabel.put("%s", "%s");' % (value, key)
+
+    for msg in sorted(fields_not_found_msgs):
+        print msg
+
+    for msg in sorted(messagekeys_not_found_msgs):
+        print msg
+
+    for msg in sorted(text_labels_not_found_msgs):
+        print msg
+    
         
         # Get selectors that are specifically for fields. (There are some
         # selectors in uispec files that are for other entity types)
