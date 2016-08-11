e304d3d507623d303174b74ebc05659ad590a8e5
John Lowe <jblowe@berkeley.edu>
2016-08-11 13:13:54 -0700

    PAHMA-1500: invoke cleanup script after tricoder upload

--- scripts/pahma/batch_barcode/tricoderlock.sh
+++ scripts/pahma/batch_barcode/tricoderlock.sh
@@ -5,6 +5,8 @@ EMAIL="pahma-tricoder@lists.berkeley.edu,cspace-support@lists.berkeley.edu"
 if mkdir $LOCKFILE; then
   echo "Locking succeeded" >&2
   /home/app_webapps/batch_barcode/import_barcode_typeR.sh &> /home/app_webapps/batch_barcode/log/all_barcode_typeR.msg
+  # alas, clean_temp.sh only works if invoked from the 'home' directory; not worth refactoring to do better yet...
+  cd /home/app_webapps/batch_barcode; ./clean_temp.sh
   rm -rf $LOCKFILE
 else
   echo "Lock failed - exit" >&2
