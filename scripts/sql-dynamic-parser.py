273bdb6828312dcb6abd37dc7bdf990136f498fb
cesarvh <cesarv.h@berkeley.edu>
2016-11-09 13:48:02 -0800

    added checks to make sure museum is input correctly

--- scripts/sql-dynamic-parser.py
+++ scripts/sql-dynamic-parser.py
@@ -1,6 +1,8 @@
 import sys
 from bs4 import BeautifulSoup
 
+valid_tokens = ["-m", "-i", "-o", "-d"]
+museums = ["pahma", "bampfa", "ucjeps", "botgarden", "cinefiles"]
 
 def parse(out, inp, mark, museum):
 	outfile = open(out, "w")
@@ -25,7 +27,6 @@ def parse(out, inp, mark, museum):
 
 			print(vocab_id, field_name)
 			new_value = "urn:cspace:%s.cspace.berkeley.edu:vocabularies:name(%s):item:name(%s)''%s''" % (museum, vocab_list, vocab_id, field_name)
-			# select_statement = "update %s set %s='%s' where %s='%s';\n" % (db_table, db_column, new_value, db_column, field_name)
 			select_statement = "update %s set %s='%s' where %s='%s';\n" % (db_table, db_column, new_value, db_column, field_name)
 			outfile.write(select_statement)
 
@@ -35,14 +36,17 @@ def parse(out, inp, mark, museum):
 						  
 if __name__ == "__main__":
 	args = sys.argv
-	if args[1] == "help":
+	if (len(args) > 1 and rgs[1] == "help"):
 		print ("To run the file, use the inputs: \n -m <museum_name> \n -i <input_file> \n -o <output_file> \n -d <domain_instance_vocab>")
 		sys.exit()
-	if len(args) < 9:
+	elif len(args) < 9:
 		print ("One or more inputs missing: -m <museum_name> -i <input_file> -o <output_file> -d <domain_instance_vocab>. \nFix or run with 'help' for more info.")
 		sys.exit()
 	else:
 		museum = args[args.index("-m") + 1]
+		if museum not in museums:
+			print ("Unknown museum %s" % museum)
+			sys.exit()
 		infile = args[args.index("-i") + 1]
 		outfile = args[args.index("-o") + 1]
 		markup = args[args.index("-d") + 1]
