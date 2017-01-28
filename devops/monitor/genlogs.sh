2bbdf4ebda5e2ac90ce7c88343c128d2e163854c
John Lowe <jblowe@berkeley.edu>
2017-01-27 16:50:52 -0800

    NOJIRA: update the webapp log analysis script to filter out Nagios monitoring queries.

--- devops/monitor/genlogs.sh
+++ devops/monitor/genlogs.sh
@@ -1,7 +1,7 @@
 t=$1
 grep -h INFO ~/${t}/logs/logfile.txt* | grep -v AuthN | grep -v accountperms | grep -v delete| perl -pe 's/\[(.*?) (.*?)\] (\w+) \[(.*?)\] (.*?) :: (.*?) :: (.*?)( :: .*?)?/${t}\t\1\t\2\t\3\t\4\t\5\t\6\t\7\t\8/'  > ${t}.temp1
 grep -h DEBUG ~/${t}/logs/logfile.txt* | grep -v 'could not authenticate' | perl -pe 's/\[(.*?) (.*?)\] (\w+) \[(.*?)\] User: (.*) authenticated with Host: (.*)/${t}\t\1\t\2\t\3\t\4\t\5\t\6/' >> ${t}.temp1
-cat ${t}.temp1 ${t}.django.log | sort -u > ${t}.temp2
+cat ${t}.temp1 ${t}.django.log | perl -ne 'print unless /text:\(\+(rotogravure|Monterey|prominent|preserve|glazed)\)/' | sort -u > ${t}.temp2
 mv ${t}.temp2 ${t}.django.log
 rm ${t}.temp*
 cut -f5 ${t}.django.log | perl -pe 's/:\d+//;s/.(views|authn|utils)//' | sort |uniq -c | sort -rn | head -20 > ${t}.logsummary.txt
