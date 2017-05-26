e4108a17b490be386d6f224ed65c9f3c7adbae29
John Lowe <jblowe@berkeley.edu>
2017-05-25 17:42:28 -0700

    BAMPFA-525: consolidate all the SQL associated with the CineFile site in one place: cinefiles_jsp repo

--- functions/cinefiles/getdispl.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION cinefiles_denorm.getdispl(text)
-    RETURNS text
-    LANGUAGE sql
-    IMMUTABLE STRICT
-AS $function$
-    SELECT regexp_replace($1, '^.*\)''(.*)''$', '\1')
-$function$;
-
-GRANT EXECUTE ON FUNCTION cinefiles_denorm.getdispl(text) to public;
