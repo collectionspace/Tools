74aee2a3231a68daf189aa420365c1b99b7c8685
John Lowe <jblowe@berkeley.edu>
2017-05-31 11:30:15 -0700

    BAMPFA-529: tweak this view creation script to create OR replace

--- datasources/bampfa/bampfa_metadata_v.sql
+++ datasources/bampfa/bampfa_metadata_v.sql
@@ -4,7 +4,7 @@
 -- 8/7/2016 incorporated Lam's changes per BAMPFA-465 and fix to artistorigin per BAMPFA-495
 
 
-create view piction.bampfa_metadata_v as
+create or replace view piction.bampfa_metadata_v as
  SELECT h1.name AS objectcsid,
     co.objectnumber AS idnumber,
     cb.sortableeffectiveobjectnumber AS sortobjectnumber,
