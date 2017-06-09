166940b2ee49df9fb64556342e070d2d85116bb8
John Lowe <jblowe@berkeley.edu>
2017-06-09 09:41:29 -0700

    CSPACE-6326: media are now cleaned up

--- devops/cluedo/cleanup.sh
+++ devops/cluedo/cleanup.sh
@@ -1,8 +1,13 @@
-
+#
+# remove records created by last run
+#
 personauthorities=`./getauthoritycsid.sh personauthorities`
 locationauthorities=`./getauthoritycsid.sh locationauthorities`
 
-cut -f4 personauthorities.created.csv | perl -pe 's/\r//' | ./helper.sh delete $personauthorities personauthorities
-cut -f4 locationauthorities.created.csv | perl -pe 's/\r//' | ./helper.sh delete $locationauthorities locationauthorities
 cut -f4 collectionobjects.created.csv | perl -pe 's/\r//' | ./helper.sh delete "" collectionobjects
 cut -f4 movements.created.csv | perl -pe 's/\r//' | ./helper.sh delete "" movements
+cut -f4 media.created.csv | perl -pe 's/\r//' | ./helper.sh delete "" media
+
+cut -f4 personauthorities.created.csv | perl -pe 's/\r//' | ./helper.sh delete $personauthorities personauthorities
+cut -f4 locationauthorities.created.csv | perl -pe 's/\r//' | ./helper.sh delete $locationauthorities locationauthorities
+
