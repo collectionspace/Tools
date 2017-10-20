bae44c2a3d85f567c4063746a9ac13b1ffae0a80
John Lowe <jblowe@berkeley.edu>
2017-10-20 09:12:34 -0700

    UCJEPS-622: 3rd command line arg is now list of names to check

--- scripts/ucjeps/cspace-fims-integration/extractFromAuthority.py
+++ scripts/ucjeps/cspace-fims-integration/extractFromAuthority.py
@@ -20,6 +20,13 @@ numberofitems = len(items)
 # if numberofitems > numberWanted:
 #    items = items[:numberWanted]
 
+if len(sys.argv) >= 4:
+    file_of_names2check = csv.reader(codecs.open(sys.argv[3], 'r', "utf-8"), delimiter='\t')
+    names2check = [line[0].strip() for line in file_of_names2check]
+    check_names = True
+else:
+    check_names = False
+
 sequence_number = 0
 for i in items:
     sequence_number += 1
@@ -28,4 +35,11 @@ for i in items:
     termDisplayName = extractTag(i, 'termDisplayName')
     refName = extractTag(i, 'refName')
     updated_at = extractTag(i, 'updatedAt')
+    # if we were given a specific list of names, only write those ones out
+    if check_names:
+        if not termDisplayName in names2check:
+            continue
+    else:
+        pass
+
     cspaceCSV.writerow([sequence_number, csid, termDisplayName, refName, updated_at])
